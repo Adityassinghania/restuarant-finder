efd858d1e27cc1683d964736391c71d9ca224051
Yunseong Koo <yunseong.koo@sjsu.edu>
2022-04-14 14:42:44 -0700

    added businesses and users data

--- db.py
+++ db.py
@@ -4,18 +4,45 @@ from pymongo import MongoClient, InsertOne
 
 client = pymongo.MongoClient("mongodb://ec2-18-205-105-6.compute-1.amazonaws.com:27017")
 db = client.test
-collection = db.yelp_reviews
-requesting = []
 
+DOCUMENT_COUNT = 150
+
+requesting = []
+collection = db.yelp_reviews
 with open(r"./yelp_academic_dataset_review.json") as f:
     for jsonObj in f:
-        if len(requesting) < 30:
+        if len(requesting) < DOCUMENT_COUNT:
             myDict = json.loads(jsonObj)
             requesting.append(InsertOne(myDict))
         else:
             break
 
 result = collection.bulk_write(requesting)
+
+requesting = []
+collection = db.yelp_users
+with open(r"./yelp_academic_dataset_user.json") as f:
+    for jsonObj in f:
+        if len(requesting) < DOCUMENT_COUNT:
+            myDict = json.loads(jsonObj)
+            requesting.append(InsertOne(myDict))
+        else:
+            break
+
+result = collection.bulk_write(requesting)
+
+requesting = []
+collection = db.yelp_businesses
+with open(r"./yelp_academic_dataset_business.json") as f:
+    for jsonObj in f:
+        if len(requesting) < DOCUMENT_COUNT:
+            myDict = json.loads(jsonObj)
+            requesting.append(InsertOne(myDict))
+        else:
+            break
+
+result = collection.bulk_write(requesting)
+
 client.close()
 
 print("done")
