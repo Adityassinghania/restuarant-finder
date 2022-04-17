445be095c6f0d9ff5b872f5ed1eec6a795b8f700
Aditya Singhania <aditya.singhania@sjsu.edu>
2022-04-16 19:34:29 -0700

    configured mongodb

--- config.py
+++ config.py
@@ -2,3 +2,8 @@ import os
 
 class Config(object):
     SECRET_KY = os.environ.get('SECRET_KEY') or "teamray"
+
+    MONGODB_SETTINGS = {
+        'db' : 'test',
+        'host':'mongodb://ec2-18-205-105-6.compute-1.amazonaws.com:27017'
+    }
