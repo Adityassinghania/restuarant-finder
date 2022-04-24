def88ab2b17bf8543b501a6998a641e58eb145a2
Aditya Singhania <aditya.singhania@sjsu.edu>
2022-04-24 01:43:46 -0700

    added api for get restaurant by id

--- application/routes.py
+++ application/routes.py
@@ -16,6 +16,11 @@ class GetBusinessesByCity(Resource):
     def get(self,city_name):
         return jsonify(yelp_businesses.objects(city = city_name))
 
+@api.route('/restaurant/<r_id>')
+class GetBusinessesById(Resource):
+    def get(self,r_id):
+        return jsonify(yelp_businesses.objects(business_id = r_id))
+
 @api.route('/reviews/<r_id>')
 class GetReviewsByRestaurantId(Resource):
     def get(self,r_id):
