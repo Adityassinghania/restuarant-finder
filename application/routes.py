f8fdce0a715e154270bae0c2696f270edba3bc91
Ricky Kuang <ricky.kuang10@gmail.com>
2022-04-30 16:52:50 -0700

    worked on some APIs, added attributes to models

--- application/routes.py
+++ application/routes.py
@@ -5,6 +5,7 @@ from models import yelp_businesses, yelp_reviews, yelp_checkins
 from flask import jsonify, request, Response
 from flask_mongoengine import MongoEngine
 from flask_restx import Resource
+from datetime import datetime
 
 @api.route('/')
 class Home(Resource):
@@ -36,68 +37,96 @@ class GetReviewsByRestaurantId(Resource):
         res = review.save()
         return jsonify(res)
 
+# NEEDS FIXING
 @api.route('/del_short_reviews/<char_count>')
 class DeleteShortReviews(Resource):
     # use get call, add reviews to a list that don't meet a minimum, and then remove them
     # look for reviews that don't exceed a character count
     def delete(self, char_count):
-        review_objs = yelp_reviews.objects()
-        shortReviews = []
+        review_objs = yelp_reviews.objects(review_id = char_count)
+        delete_count = 0
+        deleted_reviews = []
+        deleted_reviews.append(review_objs)
+        yelp_reviews.delete(review_objs)
 
-        for review in review_objs:
-            text = review["text"]
-            if len(text) < char_count:
-                shortReviews.append(review["review_id"])
+        # for review in review_objs:
+        #     text = review["text"]
+        #     if len(text) < char_count:
+        #         delete_count += 1
+        #         deleted_reviews.append(review)
+        #         review.delete()
+            
+        #     if delete_count == 100:
+        #         break
 
-        for r_id in shortReviews:
-            delete_obj = yelp_reviews.objects(review_id = r_id)
-            delete_obj.delete()
+        # print('deleted', delete_count, 'short reviews')
+        return jsonify(deleted_reviews)
 
-# note: the trust rating is the 'elite' attribute. add the current year if it hasn't
-# been added already
-# @api.route('/user_trust/<rev_count>')
-# class UpdateUserTrustRating(Resource):
-#     def put(self,rev_count):
-#         user_objs = yelp_users.objects()
-#         users = []
+# NEEDS FIXING
+@api.route('/user_trust/<rev_count>')
+class UpdateUserTrustRating(Resource):
+    def put(self,rev_count):
+        user_objs = yelp_users.objects()
+        count = 0
+        review_threshold = int(rev_count)
+        year = datetime.now().year
 
-#         for user in user_objs:
-#             if user["review_count"] > rev_count:
-#                 users.append(user["user_id"])
+        for user in user_objs:
+            if user["review_count"] > review_threshold:
+                # print(user["review_count"])
+                if int(year) not in user["elite"]:
+                    count += 1
+                    user["elite"].append(int(year))
+                    user.save()
 
-#         for user in users:
-#             # update trust rating
-#             print("trust rating updated")
+            if count == 100:
+                break
 
-@api.route('/user_useful_rev')
+        print(count, 'users with at least', rev_count, 'trust rating')
+
+
+# NEEDS FIXING
+@api.route('/user_useful_rev/<business_id>')
 class GetUsersWithMaxUsefulReviews(Resource):
-    def get(self):
-        user_objs = yelp_users.objects().rder_by('-useful')
-        print(user_objs)
-        return jsonify(user_objs)
+    def get(self,business_id):
+        # get reviews for a specific business
+        rev_objs = yelp_reviews.objects(business_id=business_id)
+        review_users = []
+        for rev in rev_objs:
+            review_users.append(rev["user_id"])
+
+        # get users that reviewed that business
+        user_objs = yelp_users.objects().order_by('-useful')
+        user_objs = yelp_users.objects()
+        users = []
+        for user in user_objs:
+            if user["user_id"] in review_users:
+                users.append(user)
+
+        return jsonify(users)
 
 
 @api.route('/restaurants_user_match/<user_input>')
 class GetRestaurantsBasedOnUserInput(Resource):
     # fetch all reviews of restaurant and do substring match and substring comes from url. pass as a query param
-    def get(self,userinput):
+    def get(self,user_input):
         business_objs = yelp_businesses.objects()
         restaurants = []
         for restaurant in business_objs:
             # check if user input in the restaurant name
-            if userinput in restaurant["name"]:
+            if user_input.casefold() in restaurant["name"].casefold():
                 restaurants.append(restaurant)
             
-            # if not, check if the input is a certain category
-            else:
-                for i in restaurant["categories"]:
-                    if userinput in i:
-                        restaurants.append(restaurant["business_id"])
-                        break
-        
+            # # if not, check if the input is a certain category
+            # else:
+            #     for i in restaurant["categories"]:
+            #         if user_input in i:
+            #             restaurants.append(restaurant)
+            #             break
+    
         return jsonify(restaurants)
     
-
+# NEEDS FIXING
 @api.route('/insert_restaurant')
 class InsertNewRestaurant(Resource):
     def post(self):
@@ -109,29 +138,31 @@ class InsertNewRestaurant(Resource):
         res = business.save()
         return jsonify(res)
 
+# NEEDS FIXING
 @api.route('/update_review/<review_id>')
 class UpdateRestaurantReview(Resource):
     # do a .save and pass a review id. change text but not the id
     def put(self, review_id):
-        content = api.payload
+        data = api.payload
         review_obj = yelp_reviews.objects(review_id = review_id)
-        review_obj.update(text=content['text'])
+        review_obj.update(text=data['text'])
         
         review = yelp_reviews(**review_obj)
         res = review.save()
         return jsonify(res)
 
 
-
+# NEEDS FIXING
 @api.route('/add_checkin/<business_id>')
 class AddCheckIn(Resource):
     def put(self,business_id):
-        data = api.payload
+        # data = api.payload
         checkin_obj = yelp_checkins.objects(business_id = business_id)
-        checkin_dates = checkin_obj["date"]
-        checkin_new = checkin_dates + data['date']
-        checkin_obj.update(date=checkin_new)
+        # checkin_dates = checkin_obj["date"]
+        # checkin_new = checkin_dates + data['date']
+        # checkin_obj.update(date=checkin_new)
 
-        checkin = yelp_checkins(**checkin_obj)
-        res = checkin.save()
-        return jsonify(res)
+        # checkin = yelp_checkins(**checkin_obj)
+        # res = checkin.save()
+        # return jsonify(res)
+        return jsonify(checkin_obj)
