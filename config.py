e1a33eefb3185685ece37f54dd0c41c1e78bcadd
Yunseong Koo <yunseong.koo@sjsu.edu>
2022-05-02 09:17:16 -0700

    started on react client

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ class Config(object):
     SECRET_KY = os.environ.get('SECRET_KEY') or "teamray"
 
     MONGODB_SETTINGS = {
-        'db' : 'test',
+        'db' : 'testdb',
         'host':'ec2-34-238-121-163.compute-1.amazonaws.com:27017'
     }
